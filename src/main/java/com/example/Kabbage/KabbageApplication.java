bf07f4593da5267d53d5847407c5402be1c46bd9
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-07-15 17:26:24 -0400

    initial commit

---
+++ src/main/java/com/example/Kabbage/KabbageApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kabbage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabbageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KabbageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Kabbage/KabbageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kabbage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KabbageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
